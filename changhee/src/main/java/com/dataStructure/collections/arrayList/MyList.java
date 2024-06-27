8a00cf1f0c35036c821933e0f1d6cb8379b7f90f
ChangHee98 <dlackdgml2@naver.com>
2024-06-27 16:52:22 +0900

    [ArrayList] ChangHee

---
+++ changhee/src/main/java/com/dataStructure/collections/arrayList/MyList.java
@@ -0,0 +1,17 @@
+package com.dataStructure.collections.arrayList;
+
+public interface MyList<T> {
+    boolean add(T value);
+    void add(int index, T value);
+    boolean remove(Object o);
+    T remove(int index);
+    T get(int index);
+    void set(int index, T value);
+    boolean contains(Object o);
+    int indexOf(Object o);
+    int lastIndexOf(Object o);
+    int size();
+    boolean isEmpty();
+    public void clear();
+
+}
