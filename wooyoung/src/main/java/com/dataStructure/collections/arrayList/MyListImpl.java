a7211b3080dae4974698fad46c417cf1ebd61a54
wooyong99 <wulovesk@naver.com>
2024-06-28 01:28:06 +0900

    [ArrayList] WooYong 구현완료

--- wooyoung/src/main/java/com/dataStructure/collections/arrayList/MyListImpl.java
+++ wooyoung/src/main/java/com/dataStructure/collections/arrayList/MyListImpl.java
@@ -3,59 +3,69 @@ package com.dataStructure.collections.arrayList;
 public class MyListImpl<T> implements MyList<T> {
 
     private static final long serialVersionUID = 8683452581122892189L;
-    private static Object[] elementData = {};
+    private static final Object[] EMPTY_ELEMENTDATA = {};
     private static final int DEFAULT_SIZE = 10;
+    private Object[] elementData = {};
+    private int size = 0;
+
 
     public MyListImpl() {
-        elementData = new Object[0];
+        elementData = new Object[DEFAULT_SIZE];
     }
 
     @Override
     public boolean add(T value) {
-        if (elementData.length == 0) {
-            elementData = new Object[1];
-            elementData[elementData.length - 1] = value;
-
-            return true;
+        if (size == elementData.length) {
+            Object[] tmp = new Object[elementData.length * 2];
+            for (int i = 0; i < elementData.length; i++) {
+                tmp[i] = elementData[i];
+            }
+            elementData = tmp;
+            size++;
+        } else {
+            elementData[size] = value;
+            size++;
         }
-
-        Object[] tmp = new Object[elementData.length];
-
-        for (int i = 0; i < elementData.length; i++) {
-            tmp[i] = elementData[i];
-        }
-
-        elementData = new Object[elementData.length + 1];
-
-        for (int i = 0; i < tmp.length; i++) {
-            elementData[i] = tmp[i];
-        }
-
-        elementData[elementData.length - 1] = value;
-
         return true;
     }
 
     @Override
     public void add(int index, T value) {
-
+        if (index >= size || index < 0) {
+            throw new ArrayIndexOutOfBoundsException("범위를 초과했습니다.");
+        }
+        if (size == elementData.length) {
+            Object[] tmp = new Object[elementData.length * 2];
+            for (int i = 0; i < index; i++) {
+                tmp[i] = elementData[i];
+            }
+            tmp[index] = value;
+            for (int i = index + 1; i < size; i++) {
+                tmp[i] = elementData[i - 1];
+            }
+            elementData = tmp;
+            size++;
+        } else {
+            for (int i = size - 1; i > index; i--) {
+                elementData[i + 1] = elementData[i];
+            }
+            elementData[index] = value;
+            size++;
+        }
     }
 
     @Override
-    public boolean remove(T value) {
-        for (int i = 0; i < elementData.length; i++) {
-            if (elementData[i] == value) {
-                // 임시 배열 생성
+    public boolean remove(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (elementData[i] == o) {
                 Object[] tmp = new Object[elementData.length - 1];
-
                 for (int j = 0; j < i; j++) {
                     tmp[j] = elementData[j];
                 }
                 for (int j = i; j < tmp.length; j++) {
                     tmp[j] = elementData[j + 1];
                 }
-
-                elementData = tmp;
+                size--;
                 return true;
             }
         }
@@ -64,11 +74,27 @@ public class MyListImpl<T> implements MyList<T> {
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException("범위를 초과했습니다.");
+        }
+        Object[] tmp = new Object[elementData.length - 1];
+        T removeValue = (T) elementData[index];
+        for (int i = 0; i < index; i++) {
+            tmp[i] = elementData[i];
+        }
+        for (int i = index; i < tmp.length; i++) {
+            tmp[i] = elementData[i + 1];
+        }
+        elementData = tmp;
+        size--;
+        return removeValue;
     }
 
     @Override
     public T get(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayIndexOutOfBoundsException("범위를 초과했습니다.");
+        }
         return (T) elementData[index];
     }
 
@@ -79,31 +105,48 @@ public class MyListImpl<T> implements MyList<T> {
 
     @Override
     public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (elementData[i] == o) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        for (int i = 0; i < elementData.length; i++) {
+            if (elementData[i] == o) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        if (elementData[size - 1] == o) {
+            return 1;
+        }
+        return -1;
     }
 
     @Override
     public int size() {
-        return elementData.length;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public void clear() {
-
+        elementData = EMPTY_ELEMENTDATA;
+        size = 0;
     }
 }
--- wooyoung/src/test/java/com/dataStructure/collections/arrayList/MyListImplTest.java
+++ wooyoung/src/test/java/com/dataStructure/collections/arrayList/MyListImplTest.java
@@ -1,17 +1,47 @@
 package com.dataStructure.collections.arrayList;
 
+
 import java.util.Random;
+import java.util.UUID;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class MyListImplTest {
+
+public class MyListImplTest {
 
     Random rnd = new Random();
 
     @Test
-    @DisplayName("add 테스트")
-    public void add() {
+    @DisplayName("List 초기화 시, size 0을 확인합니다.")
+    public void assertInit() {
+        // given
+        MyList<Integer> list = new MyListImpl<>();
+
+        // when
+
+        // then
+        Assertions.assertEquals(list.size(), 0);
+    }
+
+    @Test
+    @DisplayName("clear 후 리스트 빈 값 여부를 확인합니다.")
+    public void assertClear() {
+        // given
+        MyList<Integer> list = new MyListImpl<>();
+        list.add(rnd.nextInt(1, 10));
+
+        // when
+        list.clear();
+
+        // then
+        Assertions.assertTrue(list.isEmpty());
+    }
+
+    @Test
+    @DisplayName("5번 반복했을 때 모두 일치하는지 확인합니다.")
+    public void assertAdd() {
         // given
         MyList<Integer> list = new MyListImpl<>();
 
@@ -27,48 +57,239 @@ class MyListImplTest {
     }
 
     @Test
-    @DisplayName("addAll 테스트")
-    public void addAll() {
+    public void addTest() {
         // given
         MyList<Integer> list = new MyListImpl<>();
-        int data = rnd.nextInt(1, 10);
+        IntStream.rangeClosed(1, 10).forEach(e -> list.add(e));
+        int insertIdx = rnd.nextInt(1, 10);
+        int insertValue = rnd.nextInt(1, 10);
 
         // when
-
-        list.add(data);
+        list.add(insertIdx, insertValue);
 
         // then
-        Assertions.assertEquals(list.get(0), data);
+        Assertions.assertEquals(list.get(insertIdx), insertValue);
+
     }
 
     @Test
-    @DisplayName("리스트 범위의 값을 조회하면 ArrayIndexOutOfBoundsException 예외를 던진다.")
-    public void assertIndexBoundExceptionByAdd() {
+    @DisplayName("DEFAULT_SIZE 길이를 넘겼을 때 정상적으로 리스트 사이즈가 1 증가하는 지 확인합니다.")
+    public void validateListSize() {
+        // given
+        MyList<Integer> list = new MyListImpl<>();
+        int defaultSize = 10;
+        IntStream.rangeClosed(1, defaultSize).forEach(e -> list.add(e));
+
+        // when
+        list.add(rnd.nextInt(1, 10));
+
+        // then
+        Assertions.assertEquals(list.size(), 11);
+    }
+
+    @Test
+    @DisplayName("n번 리스트 삽입 시, size() 값을 확인합니다.")
+    public void assertSize() {
+        // given
+        MyList<Integer> list = new MyListImpl<>();
+        int count = rnd.nextInt(2, 10);
+
+        // when
+        IntStream.rangeClosed(1, count).forEach(e -> list.add(e));
+
+        // then
+        Assertions.assertEquals(count, list.size());
+    }
+
+    @Test
+    @DisplayName("특정 값이 리스트에 존재하면 true를 반환합니다.")
+    public void assertContainsBySuccess() {
+        // given
+        MyList<Integer> list = new MyListImpl<>();
+        IntStream.rangeClosed(1, 10).forEach(e -> list.add(e));
+
+        // when
+
+        // then
+        Assertions.assertTrue(list.contains(rnd.nextInt(1, 10)));
+    }
+
+    @Test
+    @DisplayName("특정 값이 리스트에 존재하지 않으면 false를 반환합니다.")
+    public void assertContainsByFail() {
+        // given
+        MyList<Integer> list = new MyListImpl<>();
+        IntStream.rangeClosed(1, 10).forEach(e -> list.add(e));
+
+        // when
+
+        // then
+        Assertions.assertFalse(list.contains(11));
+    }
+
+    @Test
+    @DisplayName("index 범위를 벗어나면 ArrayIndexOutOfBoundsException 예외를 던집니다.")
+    public void assertGetByIndexBounded() {
         // given
         MyList<Integer> list = new MyListImpl<>();
         int data = rnd.nextInt();
         list.add(data);
 
         // when
-        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> list.get(list.size()));
 
         // then
+        Assertions.assertThrowsExactly(ArrayIndexOutOfBoundsException.class,
+            () -> list.get(list.size()), "범위를 초과했습니다.");
     }
 
-//    @Test
-//    public void remove() {
-//        // given
-//        MyList<Integer> list = new MyListImpl<>();
-//        int size = 10;
-//        for (int i = 1; i < size; i++) {
-//            list.add(i);
-//        }
-//
-//        // when
-//        list.remove(3);
-//
-//        // then
-//        System.out.println(list.size());
-//    }
+    @Test
+    @DisplayName("index가 0보다 작으면 ArrayIndexOutOfBoundsException 예외를 던집니다.")
+    public void assertGetByNonNegative() {
+        // given
+        MyList<Integer> list = new MyListImpl<>();
+        int data = rnd.nextInt();
+        list.add(data);
 
+        // when
+
+        // then
+        Assertions.assertThrowsExactly(ArrayIndexOutOfBoundsException.class,
+            () -> list.get(-1), "범위를 초과했습니다.");
+    }
+
+    @Test
+    @DisplayName("index값으로 삭제한 데이터를 검증합니다.")
+    public void assertRemove() {
+        // given
+        MyList<Integer> list = new MyListImpl<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        int expect = list.get(3);
+
+        // when
+        Integer actual = list.remove(3);
+
+        // then
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 Object를 삭제하면 false를 반환합니다.")
+    public void assertRemoveByFail() {
+        // given
+        MyList<String> list = new MyListImpl<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(UUID.randomUUID().toString());
+        }
+
+        // when
+        boolean result = list.remove("hello");
+
+        // then
+        Assertions.assertFalse(result);
+        Assertions.assertEquals(10, list.size());
+    }
+
+    @Test
+    @DisplayName("존재하는 Object를 삭제하면 true를 반환합니다.")
+    public void assertRemoveBySuccess() {
+        // given
+        MyList<String> list = new MyListImpl<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(UUID.randomUUID().toString());
+        }
+
+        // when
+        boolean result = list.remove(list.get(0));
+
+        // then
+        Assertions.assertTrue(result);
+        Assertions.assertEquals(9, list.size());
+    }
+
+    @Test
+    @DisplayName("index 값이 size 보다 크면 ArrayIndexOutOfBoundsException 예외를 던집니다.")
+    public void assertRemoveByMaxLength() {
+        // given
+        MyList<Integer> list = new MyListImpl<>();
+        int data = rnd.nextInt();
+        list.add(data);
+
+        // when
+
+        // then
+        Assertions.assertThrowsExactly(ArrayIndexOutOfBoundsException.class,
+            () -> list.remove(list.size()), "범위를 초과했습니다.");
+    }
+
+    @Test
+    @DisplayName("index 값이 0보다 작으면 ArrayIndexOutOfBoundsException 예외를 던집니다.")
+    public void assertRemoveByIndexNonNegative() {
+        // given
+        MyList<Integer> list = new MyListImpl<>();
+        int data = rnd.nextInt();
+        list.add(data);
+
+        // when
+
+        // then
+        Assertions.assertThrowsExactly(ArrayIndexOutOfBoundsException.class,
+            () -> list.remove(-1), "범위를 초과했습니다.");
+    }
+
+    @Test
+    @DisplayName("특정 데이터의 index 값을 검증합니다.")
+    public void assertIndexOfSuccess() {
+        // given
+        MyList<String> list = new MyListImpl<>();
+        IntStream.rangeClosed(1, 10).forEach(e -> list.add(UUID.randomUUID().toString()));
+
+        // when
+
+        // then
+        int index = rnd.nextInt(1, 10);
+        String assertData = list.get(index);
+        Assertions.assertEquals(list.indexOf(assertData), index);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 데이터 인덱스를 조회하면 -1을 반환한다.")
+    public void assertIndexOfFail() {
+        // given
+        MyList<String> list = new MyListImpl<>();
+        IntStream.rangeClosed(1, 10).forEach(e -> list.add(UUID.randomUUID().toString()));
+
+        // when
+
+        // then
+        Assertions.assertEquals(-1, list.indexOf("아무 데이터나 넣어"));
+    }
+
+    @Test
+    @DisplayName("마지막 데이터가 일치하면 1을 반환한다.")
+    public void assertLastIndexOfSuccess() {
+        // given
+        MyList<String> list = new MyListImpl<>();
+        IntStream.rangeClosed(1, 10).forEach(e -> list.add(UUID.randomUUID().toString()));
+
+        // when
+
+        // then
+        String assertData = list.get(9);
+        Assertions.assertEquals(list.lastIndexOf(assertData), 1);
+    }
+
+    @Test
+    @DisplayName("마지막 데이터가 불일치하면 -1을 반환한다.")
+    public void assertLastIndexOfFail() {
+        // given
+        MyList<String> list = new MyListImpl<>();
+        IntStream.rangeClosed(1, 10).forEach(e -> list.add(UUID.randomUUID().toString()));
+
+        // when
+
+        // then
+        Assertions.assertEquals(list.lastIndexOf("아무데이터나넣어"), -1);
+    }
 }
