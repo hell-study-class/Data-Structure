d6f51aa5fbfb4f70f59741f56d2e7821797bbef2
MeangSung <aoddlsgh98@gmail.com>
2024-07-22 18:03:14 +0900

    [LinkedListQueue] Inho

---
+++ inho/src/main/java/com/dataStructure/LinkedQueue.java
@@ -0,0 +1,80 @@
+package com.dataStructure;
+
+public class LinkedQueue<E> implements CustomQueueBasic<E> {
+
+    class Node<E> {
+        E data;
+        Node<E> next;
+
+        public Node(E data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+    public LinkedQueue() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    @Override
+    public boolean offer(E e) {
+        Node<E> newNode = new Node<>(e);
+        if (head == null) {
+            head = newNode;
+        }else {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        size++;
+
+        return true;
+    }
+
+    @Override
+    public E poll() {
+        if (head == null) {
+            return null;
+        }
+        Node<E> temp = head;
+        E result = temp.data;
+
+        head = temp.next;
+
+        temp.next = null;
+        temp.data = null;
+        size--;
+        return result;
+    }
+
+    @Override
+    public E peek() {
+        if (head == null) {
+            return null;
+        }
+        return head.data;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node<E> temp = head;
+        while (temp != null) {
+            sb.append(temp.data);
+            sb.append(", ");
+            temp=temp.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public int size() {
+        return size;
+    }
+}
